81a0bc67a450f0c76b90048bff61c9eec5b9b3b1
HyeseongMo <hyeseong0914@naver.com>
2022-10-14 21:11:08 +0900

    [FEAT] bs58 패키지 추가 및 velasWeb3 샘플 코드 작업

--- src/Pages/Popup/example.js
+++ src/Pages/Popup/example.js
@@ -1,3 +1,7 @@
+import * as velasWeb3 from '@velas/web3';
+
 export default function () {
-  alert('Hi! (find me on src/js/popup/example.js)');
+  const configKey = new velasWeb3.PublicKey('Config1111111111111111111111111111111111111');
+  console.log(configKey.toBase58());
+  // alert('Hi! (find me on src/js/popup/example.js)');
 }
