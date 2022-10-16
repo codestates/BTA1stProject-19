3f012ee9a2393390dc6c6d64018abb6cb8fd00b7
HyeseongMo <hyeseong0914@naver.com>
2022-10-16 21:31:48 +0900

    [FEAT] 송금 결과에 따른 모달 처리

--- src/component/transaction_result_modal_component.jsx
+++ src/component/transaction_result_modal_component.jsx
@@ -6,14 +6,16 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
-  DialogTitle, Link, makeStyles,
-  Typography
-} from "@material-ui/core";
-import {useRecoilState} from "recoil";
-import {recoilNetWork} from "../states/recoilPageState";
+  DialogTitle,
+  Link,
+  makeStyles,
+  Typography,
+} from '@material-ui/core'
+import { useRecoilState } from 'recoil'
+import { recoilNetWork } from '../states/recoilPageState'
 
-const TransactionResultModal = (props) => {
-  const {status, transactionHash, handleClose, open} = props
+const TransactionResultModal = props => {
+  const { status, transactionHash, handleClose, open } = props
   const [network, setNetwork] = useRecoilState(recoilNetWork)
 
   const getTransactionHashLink = () => {
@@ -21,7 +23,7 @@ const TransactionResultModal = (props) => {
     return `${networkExplorer}/tx/${transactionHash}?cluster=testnet`
   }
 
-  const preventDefault = (event) => event.preventDefault();
+  const preventDefault = event => event.preventDefault()
 
   const classes = makeStyles(() => ({
     width: {
@@ -35,21 +37,22 @@ const TransactionResultModal = (props) => {
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
-      classname = {classes.width}
+      className={classes.width}
     >
-      <DialogTitle id="alert-dialog-title">{"트랜잭션 처리 결과"}</DialogTitle>
+      <DialogTitle id="alert-dialog-title">{'트랜잭션 처리 결과'}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          {
-            status ?
-              <Box>
-                <Typography>트랜잭션 처리 성공</Typography><br/>
-                <a href={getTransactionHashLink()} target="_blank">
-                  트랜잭션 보러가기
-                </a>
-              </Box> :
-              <Box>트랜잭션 처리 실패</Box>
-          }
+          {status ? (
+            <Box>
+              <Typography>트랜잭션 처리 성공</Typography>
+              <br />
+              <a href={getTransactionHashLink()} target="_blank">
+                트랜잭션 보러가기
+              </a>
+            </Box>
+          ) : (
+            <Box>트랜잭션 처리 실패</Box>
+          )}
         </DialogContentText>
       </DialogContent>
       <DialogActions>
